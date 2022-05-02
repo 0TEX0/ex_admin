587d02d16554b5578ea67f1c1d956c570da0d78c
0TEX0 <94195095+0TEX0@users.noreply.github.com>
2022-05-02 20:32:21 +0200

    ex_admin

---
+++ ex_admin/config.lua
@@ -0,0 +1,39 @@
+
+--  ___ __  _      ___  ___  __ __  _  _ _ 
+-- | __>\ \/      | . || . \|  \  \| || \ |
+-- | _>  \ \  ___ |   || | ||     || ||   |
+-- |___>_/\_\|___||_|_||___/|_|_|_||_||_\_|
+                                         
+
+-------- EXEMPLE ------------------------------
+-- ↓ LA LOGS (pas toucher!!)↓      ↓WEEBOK↓
+--        Staffmodeon    =      "CHANGER",
+------------------------------------------------
+
+--------- LOGS ---------
+Config = {
+    webhook = {
+        Staffmodeon = "TON WEBHOOK ICI",      -- MODE STAFF ON
+        Staffmodeoff = "TON WEBHOOK ICI",     -- MODE STAFF OFF
+        Jail = "TON WEBHOOK ICI",
+        UnJail = "TON WEBHOOK ICI",
+        teleport = "TON WEBHOOK ICI",
+        teleportTo = "TON WEBHOOK ICI",
+        revive = "TON WEBHOOK ICI",
+        teleportcoords = "TON WEBHOOK ICI",
+        teleporttoit = "TON WEBHOOK ICI",
+        message = "TON WEBHOOK ICI",
+        annonce = "TON WEBHOOK ICI",
+        SavellPlayerAuto = "TON WEBHOOK ICI",-- SAVE TOUT LES JOUEURS
+        clearInv = "TON WEBHOOK ICI",        -- CLEAR INVENTAIRE
+        clearLoadout = "TON WEBHOOK ICI",    -- CLEAR ARME
+        WipePlayer = "TON WEBHOOK ICI",
+        kick = "TON WEBHOOK ICI",
+        SendLogs = "TON WEBHOOK ICI",        -- GENERAL LOGS
+        report = "TON WEBHOOK ICI"
+    }
+}
+
+------- JAIL ------
+Config.JailBlip     = {x = 256.83, y = -784.37, z = 30.45}  -- coordonnées de la UNJAIL
+Config.JailLocation = {x = 1641.64, y = 2571.08, z = 45.50} -- coordonnées de la JAIL 
